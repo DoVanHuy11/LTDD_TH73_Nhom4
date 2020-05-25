253cec3edefb54cd88c73e2b82159c8bb7c3bfef
thuthaokg1999 <thuthaokg1999@gmail.com>
2020-05-25 20:01:56 +0700

    Revert "Update Chi Tiết sản phẩm"
    This reverts commit 2a85d7e82dd479e2eff044808304cbbf057250f3.

--- Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
+++ Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.fashion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_detail);
+        setContentView(R.layout.activity_main);
     }
-
 }
