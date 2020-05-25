2a85d7e82dd479e2eff044808304cbbf057250f3
thuthaokg1999 <thuthaokg1999@gmail.com>
2020-05-25 17:29:01 +0700

    Update Chi Tiết sản phẩm
    update

--- Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
+++ Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.fashion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_product_detail);
     }
+
 }
