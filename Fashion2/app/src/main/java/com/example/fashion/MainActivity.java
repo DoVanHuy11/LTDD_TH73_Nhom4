97673601f972062a4b2968e46e7c74401092c264
trai <ngoctraiho98@gmail.com>
2020-06-03 23:03:30 +0700

    use ListView

--- Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
+++ Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
@@ -6,16 +6,40 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import myadapter.ProductAdapter;
+
 public class MainActivity extends AppCompatActivity {
+    private GridView listProduct;
+
     Button btnAdd;
+
+    String[] maintitle={
+            "San pham 1", "San pham 2", "San pham 3", "San pham 4", "San pham 5", "San pham 6"
+    };
+
+    String[] subtitle={
+            "Subtitle 1", "Subtitle 2", "Subtitle 3",  "Subtitle 4", "Subtitle 5", "Subtitle 6",
+    };
+
+    Integer[] imgId={
+            R.drawable.img2, R.drawable.img3, R.drawable.img4,
+            R.drawable.img5, R.drawable.img6, R.drawable.img7,
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProductAdapter adapter = new ProductAdapter(this , maintitle, subtitle, imgId);
+        listProduct = (GridView) findViewById(R.id.listview);
+        listProduct.setAdapter(adapter);
     }
 
     @Override
@@ -36,5 +60,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 
 }
