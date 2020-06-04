a39b117bae1404fff63ea9facbc1c5edb52193b1
trai <ngoctraiho98@gmail.com>
2020-06-04 09:45:51 +0700

    Update trang chu

--- Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
+++ Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
@@ -14,16 +14,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import myadapter.ProductAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private GridView listProduct;
+    private ListView listProduct;
 
     Button btnAdd;
 
     String[] maintitle={
-            "San pham 1", "San pham 2", "San pham 3", "San pham 4", "San pham 5", "San pham 6"
+            "Daddy Shirt", "Barbed Wire Shirt", "Warning Sleeve", "Warning Tee Đen",  "Color Block Tee", "Rat TShirt - RTS"
     };
 
     String[] subtitle={
-            "Subtitle 1", "Subtitle 2", "Subtitle 3",  "Subtitle 4", "Subtitle 5", "Subtitle 6",
+            "357.000", "439.000", "365.000",  "512.000", "355.000", "455.000",
     };
 
     Integer[] imgId={
@@ -31,14 +31,13 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.img5, R.drawable.img6, R.drawable.img7,
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ProductAdapter adapter = new ProductAdapter(this , maintitle, subtitle, imgId);
-        listProduct = (GridView) findViewById(R.id.listview);
+        listProduct = (ListView) findViewById(R.id.listview);
         listProduct.setAdapter(adapter);
     }
 
