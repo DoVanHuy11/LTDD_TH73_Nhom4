ba2b9a14486083bd0bb6b1f404411ff27536ba7d
trai <ngoctraiho98@gmail.com>
2020-06-04 10:44:27 +0700

    update ViewPager

--- Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
+++ Fashion2/app/src/main/java/com/example/fashion/MainActivity.java
@@ -2,22 +2,35 @@ package com.example.fashion;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import myadapter.ImageAdapter;
 import myadapter.ProductAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    ///
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 500; //delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 3000;  // time in milliseconds between successive task executions.
+
     private ListView listProduct;
-
     Button btnAdd;
-
     String[] maintitle={
             "Daddy Shirt", "Barbed Wire Shirt", "Warning Sleeve", "Warning Tee Đen",  "Color Block Tee", "Rat TShirt - RTS"
     };
@@ -31,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.img5, R.drawable.img6, R.drawable.img7,
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +53,34 @@ public class MainActivity extends AppCompatActivity {
         ProductAdapter adapter = new ProductAdapter(this , maintitle, subtitle, imgId);
         listProduct = (ListView) findViewById(R.id.listview);
         listProduct.setAdapter(adapter);
+
+        //////////////
+
+        final ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        ImageAdapter adapter1 = new ImageAdapter(this );
+        mViewPager.setAdapter(adapter1);
+
+        ////
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == 3) {
+                    currentPage = 0;
+                }
+                mViewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer.schedule(new TimerTask() { // task to be scheduled
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, DELAY_MS, PERIOD_MS);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
