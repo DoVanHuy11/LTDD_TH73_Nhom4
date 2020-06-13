2d695c5d939736210fa9b3983c6887e983675ec2
thuthaokg1999 <thuthaokg1999@gmail.com>
2020-06-13 19:25:23 +0700

    product_detail_update(3)

--- Fashion2/app/src/main/java/com/example/fashion/ProductDetailActivity.java
+++ Fashion2/app/src/main/java/com/example/fashion/ProductDetailActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -23,6 +25,7 @@ public class ProductDetailActivity extends AppCompatActivity {
 
     private Spinner spinner;
     Button btnAddCart0, btnAddCart1;
+    ListView listProductDetail;
 
     Integer [] imgid = {
             R.drawable.img1, R.drawable.img2, R.drawable.img3,R.drawable.img4,
@@ -35,10 +38,9 @@ public class ProductDetailActivity extends AppCompatActivity {
     };
 
     String [] subtitle ={
-            "galaxy", "is", "amazing", "I",
-            "want", "to", "close", "it"
+            "Simple description", "Simple description", "Simple description", "Simple description",
+            "Simple description", "Simple description", "Simple description", "Simple description"
     };
-    private ListView listProductDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +52,11 @@ public class ProductDetailActivity extends AppCompatActivity {
         ProductDetail_ImageAdapter adapter1 = new ProductDetail_ImageAdapter(this);
         mViewPager.setAdapter(adapter1);
 
-        AnhXa();
+        getValues();
         CatchEventSpinner();
         CatchEventButtonClick();
+        CatchOnItemListView();
+        SetFocusForListView();
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
@@ -61,10 +65,6 @@ public class ProductDetailActivity extends AppCompatActivity {
         ProductDetail_ListViewAdapter adapter2 = new ProductDetail_ListViewAdapter(this, maintitle, subtitle, imgid);
         listProductDetail = (ListView)findViewById(R.id.listProductDetail);
         listProductDetail.setAdapter(adapter2);
-
-
-
-
     }
 
 
@@ -84,13 +84,14 @@ public class ProductDetailActivity extends AppCompatActivity {
         spinner.setAdapter(arrayAdapter);
     }
 
-    private void AnhXa(){
+    private void getValues(){
         spinner = (Spinner) findViewById(R.id.spinner);
         btnAddCart0 = (Button) findViewById(R.id.btnAddCart0);
         btnAddCart1 = (Button) findViewById(R.id.btnAddCart1);
+        listProductDetail = (ListView) findViewById(R.id.listProductDetail);
     }
-    private void CatchEventButtonClick(){
 
+    private void CatchEventButtonClick(){
         btnAddCart0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,6 +104,28 @@ public class ProductDetailActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Sản phẩm đã được thêm vào giỏ hàng!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    private void SetFocusForListView(){
+        listProductDetail.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+    }
+
+    private void CatchOnItemListView() {
+        listProductDetail.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                for(position = 0; position <= listProductDetail.getCount(); position++){
+                    Intent intent = new Intent(getApplication(), ProductDetailActivity.class);
+                    startActivity(intent);
+                    break;
+                }
+            }
+        });
     }
 }
