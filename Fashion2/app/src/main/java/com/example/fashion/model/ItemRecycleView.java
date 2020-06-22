142c94dca4703a5cee8376a15f18e4cf94f525d0
nhantruong1298 <nhantruong1298@gmail.com>
2020-06-22 23:33:34 +0700

    update ID item

--- Fashion2/app/src/main/java/com/example/fashion/model/ItemRecycleView.java
+++ Fashion2/app/src/main/java/com/example/fashion/model/ItemRecycleView.java
@@ -7,16 +7,18 @@ public class ItemRecycleView implements Serializable {
     private String Image;
     private String Name;
     private String Price;
+    private String Id;
 
 
     public ItemRecycleView() {
     }
 
-    public ItemRecycleView(String Decription, String Image, String Name, String Price) {
+    public ItemRecycleView(String Decription, String Image, String Name, String Price,String Id) {
         this.Description = Decription;
         this.Image = Image;
         this.Name = Name;
         this.Price = Price;
+        this.Id=Id;
     }
 
     public String getDescription() {
@@ -33,5 +35,9 @@ public class ItemRecycleView implements Serializable {
 
     public String getPrice() {
         return Price;
+    }
+
+    public String getId() {
+        return Id;
     }
 }
